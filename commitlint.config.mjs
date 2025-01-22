c756b737d6a92ae9335f5d3168630e20abecb258
adelpro <adelpro@gmail.com>
2025-01-22 18:16:04 +0100

    chore: add Commitizen and configure commitlint rules

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,32 +2,9 @@ const config = {
   extends: ['@commitlint/config-conventional'],
 
   rules: {
-    'header-max-length': [2, 'always', 72],
-
-    'scope-case': [2, 'always', 'lower-case'],
-
-    'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
-
-    'type-case': [2, 'always', 'lower-case'],
-
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-        'build',
-        'ci',
-        'hotfix',
-      ],
-    ],
+    'references-empty': [1, 'never'],
+    'footer-max-line-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
   },
 };
 
