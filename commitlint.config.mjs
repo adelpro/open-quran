31fe4ce221aacfdcc31cbff06988efa2bd9bc6c2
adelpro <adelpro@gmail.com>
2025-01-28 11:19:31 +0100

    chore: update pre-commit hook to include type-check and modify type-check command in package.json

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,11 +1,5 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
-
-  rules: {
-    'references-empty': [1, 'never'],
-    'footer-max-line-length': [0, 'always'],
-    'body-max-line-length': [0, 'always'],
-  },
 };
 
 export default config;
