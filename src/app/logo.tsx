0d59e25084396ecae20859fee5c8bb5df1749dff
adelpro <adelpro@gmail.com>
2025-02-09 17:01:27 +0100

    fix: fix logo error

--- src/app/logo.tsx
+++ src/app/logo.tsx
@@ -4,7 +4,13 @@ export default function Logo() {
   return (
     <>
       <div className="mt-10 flex w-full items-center justify-center transition-transform duration-200 hover:scale-105">
-        <Image src="/logo.png" alt="logo" width={200} height={200} priority />
+        <Image
+          src="/images/logo.png"
+          alt="logo"
+          width={200}
+          height={200}
+          priority
+        />
       </div>
       <div className="width-full flex items-center justify-center">
         <h1 className="text-4xl font-bold">القرآن الكريم</h1>
