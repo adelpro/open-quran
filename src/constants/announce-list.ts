a278ab52da93641eed08979467f3a2f3418354e8
adelpro <adelpro@gmail.com>
2025-02-09 14:46:47 +0100

    fix(use-torrent): add private tracker

--- src/constants/announce-list.ts
+++ src/constants/announce-list.ts
@@ -1,7 +1,6 @@
 export const announceList = [
+  'wss://tracker.openquran.us.kg',
   'wss://tracker.openwebtorrent.com',
   'wss://tracker.btorrent.xyz',
   'wss://tracker.fastcast.nz',
-  'wss://tracker.webtorrent.dev',
-  'udp://tracker.openbittorrent.com:80',
 ];
