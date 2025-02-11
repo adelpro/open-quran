0df72a7c7f6fd9a86d2cb045836ddf5dd2448d57
adelpro <adelpro@gmail.com>
2025-02-11 10:36:31 +0100

    fix(home): add logo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import Image from 'next/image';
-
 import ReciterPage from '@/components/reciter-page';
+
+import Logo from './logo';
 
 export default function Home() {
   return (
     <main className="flex h-dvh w-full flex-col items-center justify-center bg-background text-foreground">
-      <div className="mt-10 flex w-full items-center justify-center transition-transform duration-200 hover:scale-105">
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={200}
-          height={200}
-          priority
-        />
-      </div>
-      <div className="width-full flex items-center justify-center">
-        <h1 className="text-4xl font-bold">القرآن الكريم</h1>
-      </div>
+      <Logo />
       <ReciterPage id={undefined} />
     </main>
   );
