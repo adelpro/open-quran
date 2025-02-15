990543920eb7e4b27cdd10f31b95ee11a0ad9bba
adelpro <adelpro@gmail.com>
2025-02-15 19:57:35 +0100

    refactor(rsconfig): update the rsConfig file

--- src/constants/rtc-config.ts
+++ src/constants/rtc-config.ts
@@ -1,33 +1,39 @@
 import { serverConfig } from '@/utils';
+const meteredTurnServers =
+  serverConfig.METERED_USERNAME && serverConfig.METERED_CREDENTIAL
+    ? [
+        {
+          urls: [
+            'turns:global.relay.metered.ca:443?transport=tcp',
+            'turn:global.relay.metered.ca:443',
+            'turn:global.relay.metered.ca:80?transport=tcp',
+          ],
+          username: serverConfig.METERED_USERNAME,
+          credential: serverConfig.METERED_CREDENTIAL,
+        },
+      ]
+    : [];
+
+const expressTurnServers =
+  serverConfig.EXPRESSTURN_USERNAME && serverConfig.EXPRESSTURN_CREDENTIAL
+    ? [
+        {
+          urls: 'turns:relay1.expressturn.com:3478',
+          username: serverConfig.EXPRESSTURN_USERNAME,
+          credential: serverConfig.EXPRESSTURN_CREDENTIAL,
+        },
+      ]
+    : [];
 
 export const rtcConfig: RTCConfiguration = {
   iceServers: [
-    {
-      // STUN (Session Traversal Utilities for NAT)
-      urls: [
-        'stun:stun.l.google.com:19302',
-        'stun:stun.cloudflare.com',
-        'stun:global.stun.twilio.com:3478',
+    // Primary STUN
+    { urls: 'stun:global.stun.twilio.com:3478' },
+    { urls: 'stun:stun.cloudflare.com' },
+    { urls: 'stun:stun.l.google.com:19302' },
 
-        // Optional extra for redundancy:
-        // 'stun:stun1.l.google.com:19305',
-      ],
-    },
-    serverConfig.EXPRESSTURN_USERNAME &&
-      serverConfig.EXPRESSTURN_CREDENTIAL && {
-        urls: 'turns:relay1.expressturn.com:3478',
-        username: serverConfig.EXPRESSTURN_USERNAME,
-        credential: serverConfig.EXPRESSTURN_CREDENTIAL,
-      },
-    serverConfig.METERED_USERNAME &&
-      serverConfig.METERED_CREDENTIAL && {
-        urls: [
-          'turns:global.relay.metered.ca:443?transport=tcp',
-          'turn:global.relay.metered.ca:443',
-          'turn:global.relay.metered.ca:80?transport=tcp',
-        ],
-        username: serverConfig.METERED_USERNAME,
-        credential: serverConfig.METERED_CREDENTIAL,
-      },
-  ].filter(Boolean),
+    // TURN servers (prioritize TCP/TLS)
+    ...meteredTurnServers,
+    ...expressTurnServers,
+  ],
 };
