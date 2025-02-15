9e95790768c5a77d795cad982fe827e18585a73e
adelpro <adelpro@gmail.com>
2025-02-15 19:55:38 +0100

    refactor(torrent): refactored rcConfig

--- src/constants/rtc-config.ts
+++ src/constants/rtc-config.ts
@@ -1,6 +1,9 @@
+import { serverConfig } from '@/utils';
+
 export const rtcConfig: RTCConfiguration = {
   iceServers: [
     {
+      // STUN (Session Traversal Utilities for NAT)
       urls: [
         'stun:stun.l.google.com:19302',
         'stun:stun.cloudflare.com',
@@ -10,5 +13,21 @@ export const rtcConfig: RTCConfiguration = {
         // 'stun:stun1.l.google.com:19305',
       ],
     },
-  ],
+    serverConfig.EXPRESSTURN_USERNAME &&
+      serverConfig.EXPRESSTURN_CREDENTIAL && {
+        urls: 'turns:relay1.expressturn.com:3478',
+        username: serverConfig.EXPRESSTURN_USERNAME,
+        credential: serverConfig.EXPRESSTURN_CREDENTIAL,
+      },
+    serverConfig.METERED_USERNAME &&
+      serverConfig.METERED_CREDENTIAL && {
+        urls: [
+          'turns:global.relay.metered.ca:443?transport=tcp',
+          'turn:global.relay.metered.ca:443',
+          'turn:global.relay.metered.ca:80?transport=tcp',
+        ],
+        username: serverConfig.METERED_USERNAME,
+        credential: serverConfig.METERED_CREDENTIAL,
+      },
+  ].filter(Boolean),
 };
