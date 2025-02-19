99694e095d36ffc24de3095bbcf03bffdf3e5e9a
adelpro <adelpro@gmail.com>
2025-02-19 19:20:25 +0100

    refactor(pwa): update the code to be fix build errors

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 // next.config.mjs
 import withBundleAnalyzer from '@next/bundle-analyzer';
-import withSerwist from '@serwist/next';
+import withSerwistInit from '@serwist/next';
 import type { NextConfig } from 'next';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// Configure serwist options – adjust swSrc/swDest as needed.
-const serwistConfig = {
-  swSrc: 'public/sw.js', // location of your custom service worker
+const withSerwist = withSerwistInit({
+  swSrc: 'src/sw.ts',
   swDest: 'public/sw.js',
-  cacheOnFrontEndNav: true,
-  skipWaiting: true,
-  dynamicStartUrl: false, // Precache home page instead of storing it in runtime cache
-};
+  cacheOnNavigation: true,
+  disable: !isProduction,
+  register: isProduction,
+  maximumFileSizeToCacheInBytes: 100 * 1024 * 1024, // 100MB
+});
 
 const nextConfig: NextConfig = {
   reactStrictMode: !isProduction,
@@ -49,9 +49,18 @@ const nextConfig: NextConfig = {
 };
 
 // Wrap your Next.js config with serwist.
-const configWithPWA = withSerwist(serwistConfig)(nextConfig);
+const configWithPWA = withSerwist(nextConfig);
 
-// Finally, chain with bundle analyzer.
+/* How this will work
+
+* Bundle-analyzer will run only if we pass 'ANALYZER=true' to our command (yarn analyze)
+*
+* next-PWA is disabled in developement ( disable: process.env.NODE_ENV === 'development', )
+* next-PWA will run only with this command ( yarn build)
+*
+* running (yarn dev) will only pass the (config)
+*/
+
 export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })(configWithPWA);
