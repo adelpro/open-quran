02c57062476475bb4ea6b76d1890b5fe5427bb85
adelpro <adelpro@gmail.com>
2025-02-19 09:01:13 +0100

    chore(commit-lint): commit lint config update

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,19 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        'test', // Adding missing tests or correcting existing tests
+        'docs', // Documentation changes
+        'chore', // Refactoring, tooling, miscellaneous, etc.
+      ],
+    ],
+  },
 };
 
 export default config;
